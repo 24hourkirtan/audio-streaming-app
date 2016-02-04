bdf04a8f737c15f187686911ff3c497f0a297686
David <schmooferlabs@gmail.com>
2016-02-04 01:55:57 -0800

    Added installation instructions

--- www/lib/ionic-platform-web-client/src/push/es5.js
+++
@@ -1,9 +0,0 @@
-import { Push } from "./push";
-import { PushToken } from "./push-token";
-
-// Declare the window object
-window.Ionic = window.Ionic || {};
-
-// Ionic Namespace
-Ionic.Push = Push;
-Ionic.PushToken = PushToken;
