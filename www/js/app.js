4d7549730a64aec19cb37eb0e2052803acec0dce
David <schmooferlabs@gmail.com>
2016-02-04 03:00:25 -0800

    Cleaned up files

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', [
-  'ionic','ionic.service.core',
+  'ionic',
   'starter.controllers',
   'starter.services',
   'ngCordova',
