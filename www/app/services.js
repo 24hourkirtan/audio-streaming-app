77059094079aa13a6f4254a47ef9057c940ea3c2
David Li <david@Davids-Mac.local>
2016-09-25 02:47:10 -0700

    Updated iOS config

--- www/app/services.js
+++ www/app/services.js
@@ -66,7 +66,7 @@ angular.module('app.services', [])
     },
     play: function(isRadio){
       if(media)
-        media.play();
+        media.play({ playAudioWhenScreenIsLocked : true });
       state.playing = true;
       state.isRadio = isRadio;
     },
